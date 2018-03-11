b1d915bccc88f41c9135eddea328af34ae29523e
Brendan Michael Iwao Ozawa Burns <brendanmioburns@gmail.com>
2018-03-10 19:58:35 -0800

    create API methods

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,217 @@
+const api = 'http://localhost:3001'
+
+let token = localStorage.token;
+
+if (!token) {
+  token = localStorage.token = Math.random().toString(36).substr(2, 9)
+}
+
+const headers = {
+  'Accept': 'application/json',
+  'Content-Type': 'application/json',
+  'Authorization': token,
+}
+// GET /categories
+//   USAGE:
+//     Get all of the categories available for the app. List is found in categories.js.
+export const retrieveAllCategories = () => {
+  fetch(`${api}/categories`, { headers })
+  .then(res => res.json())
+  .then(data => data.categories)
+}
+
+// GET /:category/posts
+//   USAGE:
+//     Get all of the posts for a particular category
+export const retrieveAllPostsInCategory = (category) => {
+  fetch(`${api}/${category}/posts`, { headers })
+    .then(res => res.json())
+    .then(data => data)
+}
+
+// GET /posts
+//   USAGE:
+//     Get all of the posts. Useful for the main page when no category is selected.
+export const retrieveAllPosts = () => {
+  fetch(`${api}/${posts}`, { headers })
+    .then(res => res.json())
+    .then(data => data)
+}
+
+// POST /posts
+//   USAGE:
+//     Add a new post
+//
+//   PARAMS:
+//     id - UUID should be fine, but any unique id will work
+//     timestamp - timestamp in whatever format you like, you can use Date.now() if you like
+//     title - String
+//     body - String
+//     author - String
+//     category: Any of the categories listed in categories.js. Feel free to extend this list as you desire.
+export const createNewPost = (newPost) => {
+  fetch(`${api}/posts`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      ...newPost,
+      id: uuid(),
+      timestamp: Date.now(),
+    })
+  })
+  .then(res => res.json())
+  .then(data => data)
+}
+
+// GET /posts/:id
+//   USAGE:
+//     Get the details of a single post
+export const retrieveSinglePost = (post) => {
+  fetch(`${api}/posts/${post.id}`, { headers })
+    .then(res => res.json())
+    .then(data => data)
+}
+
+// POST /posts/:id
+//   USAGE:
+//     Used for voting on a post
+//   PARAMS:
+//     option - String: Either "upVote" or "downVote"
+export const voteOnPost = (post, option) => {
+  fetch(`${api}/posts/${post.id}`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      option
+    })
+  })
+  .then(res => res.json())
+  .then(data => data)
+}
+
+// PUT /posts/:id
+//   USAGE:
+//     Edit the details of an existing post
+//   PARAMS:
+//     title - String
+//     body - String
+export const editPostDetails = (post) => {
+  fetch(`${api}/posts/${post.id}`, {
+    method: 'PUT',
+    headers,
+    body: JSON.stringify({
+      title: post.title,
+      body: post.body,
+      timestamp: Date.now(),
+    })
+  })
+  .then(res => res.json())
+  .then(data => data)
+}
+
+// DELETE /posts/:id
+//   USAGE:
+//     Sets the deleted flag for a post to 'true'.
+//     Sets the parentDeleted flag for all child comments to 'true'.
+export const deletePost = (post) => {
+  fetch(`${api}/posts/${post.id}`, {
+    method: 'DELETE',
+    headers,
+  })
+  .then(res => res.json())
+  .then(data => data)
+}
+
+// GET /posts/:id/comments
+//   USAGE:
+//     Get all the comments for a single post
+export const retrieveCommentsFromSinglePost = (post) => {
+  fetch(`${api}/posts/${post.id}/comments`, { headers })
+    .then(res => res.json())
+    .then(data => data)
+}
+
+// POST /comments
+//   USAGE:
+//     Add a comment to a post
+//
+//   PARAMS:
+//     id: Any unique ID. As with posts, UUID is probably the best here.
+//     timestamp: timestamp. Get this however you want.
+//     body: String
+//     author: String
+//     parentId: Should match a post id in the database.
+//
+export const createNewComment = (newComment) => {
+  fetch(`${api}/comments`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      ...newComment,
+      id: uuid(),
+      timestamp: Date.now(),
+    })
+  })
+  .then(res => res.json())
+  .then(data => data)
+}
+
+// GET /comments/:id
+//   USAGE:
+//     Get the details for a single comment
+//
+export const retrieveSingleComment = (comment) => {
+  fetch(`${api}/comments/${comment.id}`, { headers })
+    .then(res => res.json())
+    .then(data => data)
+}
+
+// POST /comments/:id
+//   USAGE:
+//     Used for voting on a comment.
+//   PARAMS:
+//     option - String: Either "upVote" or "downVote"
+export const voteOnComment = (comment, option) => {
+  fetch(`${api}/comments/${comment.id}`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      option
+    })
+  })
+  .then(res => res.json())
+  .then(data => data)
+}
+
+// PUT /comments/:id
+//   USAGE:
+//     Edit the details of an existing comment
+//
+//   PARAMS:
+//     timestamp: timestamp. Get this however you want.
+//     body: String
+export const editCommentDetails = (comment) => {
+  fetch(`${api}/comments/${comment.id}`, {
+    method: 'PUT',
+    headers,
+    body: JSON.stringify({
+      ...comment,
+      body: comment.body,
+      timestamp: Date.now(),
+    })
+  })
+  .then(res => res.json())
+  .then(data => data)
+}
+
+// DELETE /comments/:id
+//   USAGE:
+//     Sets a comment's deleted flag to 'true'
+export const deleteComment = (comment) => {
+  fetch(`${api}/comments/${comment.id}`, {
+    method: 'DELETE',
+    headers,
+  })
+  .then(res => res.json())
+  .then(data => data)
+}
